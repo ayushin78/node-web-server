17b925258546961c6f20b9c0a639c92d2ea4bed1
Ayushi <ayushin78@gmail.com>
2018-04-28 21:31:26 +0530

    add projects.hbs for portfolio

--- server.js
+++ server.js
@@ -10,16 +10,15 @@ app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials')
 
-app.use((req, res, next)=>{
-	var now = new Date().toString();
-	var log = `${now}, ${req.method}, ${req.url}`;
-	console.log(log);
-	next();
-});
+// app.use((req, res, next)=>{
+// 	var now = new Date().toString();
+// 	var log = `${now}, ${req.method}, ${req.url}`;
+// 	console.log(log);
+// 	next();
+// });
 
 // app.use((req, res, next) => {
 // 	res.render('maintenance.hbs');
-	
 // });
 
 app.use(express.static(__dirname + '/public'));
@@ -28,6 +27,13 @@ app.get('/about', (req, res) => {
 	res.render('about.hbs', {
 		pageTitle : 'ABout Page',
 		currentYear : new Date().getFullYear()
+	});
+});
+
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'projects',
+		currentYear: new Date().getFullYear()
 	});
 });
 
