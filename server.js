e108317083a6acfc5a226366c9cc5b12c977a50b
Ayushi <ayushin78@gmail.com>
2018-04-28 21:10:59 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -42,4 +43,6 @@ app.get('/bad', ((req, res) => {
 		errorMessage : 'not found'
 	});
 }));
-app.listen(3000);
+app.listen(port, () => {
+	console.log(`The server is live at ${port}`);
+});
